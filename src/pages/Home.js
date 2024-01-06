05f8e7b28ae31b518ec2dc6f0407c006862dc891
Sarid10 <saridqureshi299@gmail.com>
2024-01-06 19:17:24 +0530

    Added Sidebar

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import {
+  Container,
+  Stack,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Icon,
+  IconButton,
+  createIcon,
+  IconProps,
+  useColorModeValue,
+} from '@chakra-ui/react'
+
+import Navbar from '../components/Navbar'
+
+export default function CallToActionWithVideo() {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
